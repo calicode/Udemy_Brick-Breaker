8b3a8a57f4749ab36700e816b66f2a7c7d492cdc
Andrew Schmidthuber <aschmidthuber@gmail.com>
2017-01-15 14:40:44 -0600

    basic colliders, progress on level manager

--- Assets/Scripts/LevelManager.cs
+++ Assets/Scripts/LevelManager.cs
@@ -3,14 +3,24 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class LevelManager : MonoBehaviour {
-	public void LoadLevel(string name) {
-		Debug.Log ("Loading level " +name);
+public class LevelManager : MonoBehaviour
+{
+	public static void LoadLevel (string name)
+	{
+		Debug.Log ("Loading level " + name);
 		SceneManager.LoadScene (name);
 
 	}
 
-	public void QuitGame() {
+	public void LoadLevelWrapper (string name)
+	{
+		LoadLevel (name);
+
+
+	}
+
+	public void QuitGame ()
+	{
 		Debug.Log ("quit called");
 	
 		Application.Quit ();
---
+++ Assets/Scripts/LoseCollider.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class LoseCollider : MonoBehaviour
+{
+	void OnTriggerEnter2D (Collider2D trigger)
+	{
+		print ("Trigger"); 
+		LevelManager.LoadLevelWrapper ("Level_01");
+
+
+	}
+
+	void OnCollisionEnter2D (Collision2D collision)
+	{
+		print ("Collision");
+		print (collision.transform);
+	}
+
+
+
+
+}
+
